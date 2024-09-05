2079fc2aed5ce3461fab88e04c7c2e3307ba0d85
SergeiGor <sgorobin@mail.ru>
2024-09-05 16:02:03 +0300

    Add files via upload

---
+++ module_10_5.py
@@ -0,0 +1,43 @@
+import multiprocessing
+import datetime
+def read_info(name):
+    all_data = []
+    with open(name,'r',encoding='utf-8') as faile:
+        data = faile.readline()
+        while data:
+            data = faile.readline()
+            all_data.append(data)
+
+filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+
+# Линейный вызов
+
+# start = datetime.datetime.now()
+# read_info(filenames[0])
+# read_info(filenames[1])
+# read_info(filenames[2])
+# read_info(filenames[3])
+# end = datetime.datetime.now()
+# print(end-start)
+
+# Многопроцессный
+
+if __name__ == '__main__':
+    start = datetime.datetime.now()
+    with multiprocessing.Pool() as pool:
+        pool.map(read_info, filenames)
+    end = datetime.datetime.now()
+    print(end - start)
+
+
+
+
+
+
+
+
+
+
+
+
