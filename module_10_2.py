bbb4f368f9de3382401de5429c17d6346afc27d6
SergeiGor <sgorobin@mail.ru>
2024-09-11 13:46:26 +0300

    Add files via upload

--- module_10_2.py
+++ module_10_2.py
@@ -2,7 +2,6 @@ from threading import Thread
 from time import sleep
 
 
-
 class Knight(Thread):
     def __init__(self, name, power):
         super().__init__()
@@ -10,29 +9,26 @@ class Knight(Thread):
         self.power = power
 
     def run(self):
-        print(f"<{name}, на нас напали!")
-        for i in rage(100-power):
-            print('идет бой')
+        print(f"<{self.name}, на нас напали!")
+        count = 100
+        day =0
+        while count > 0:
+            count -= self.power
+            day += 1
+            if count < self.power:
+                count = 0
+            print(f'{self.name}, сражается {day} дня (день)..., осталось {count} воинов')
+            sleep(1)
+        print(f'{self.name}, одержал победу спустя {day} дня (день)')
 
 
 
-
-
-
-
-# Создание класса
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight("Sir Galahad", 20)
-# Запуск потоков и остановка текущего
-# Вывод строки об окончании сражения
 
+first_knight.start()
+second_knight.start()
+first_knight.join()
+second_knight.join()
 
-# Создайте класс , наследованный от Thread, объекты которого будут обладать следующими свойствами:
-# Атрибут  - имя рыцаря. ()
-# Атрибут power - сила рыцаря. ()
-# А также метод run, в котором рыцарь будет сражаться с врагами:
-# При запуске потока должна выводится надпись .
-# Рыцарь сражается до тех пор, пока не повергнет всех врагов (у всех потоков их 100).
-# В процессе сражения количество врагов уменьшается на power текущего рыцаря.
-# По прошествию 1 дня сражения (1 секунды) выводится строка "<Имя рыцаря> сражается <кол-во дней>..., осталось <кол-во воинов> воинов."
-# После победы над всеми врагами выводится надпись "<Имя рыцаря> одержал победу спустя <кол-во дней> дней(дня)!"
+print('Все бытвы закончились!')
