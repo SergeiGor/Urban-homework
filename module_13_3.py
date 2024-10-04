dc0d5b8e7f78ed09c58bf9b39a790372382c3864
SergeiGor <sgorobin@mail.ru>
2024-10-04 18:46:03 +0300

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,32 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+
+api = '8050141538:AAEQjto5Qn5-TnOnqeJZ7E-QB88d90o4WVo'
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    #print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_massages(message):
+    #print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+
+#
+# - печатает строку в консоли  . Запускается только когда написана команда '/start' в чате с ботом. (используйте соответствующий декоратор)
+# (message)
+# - печатает строку в консоли .
+# Запускается при любом обращении не описанном ранее. (используйте соответствующий декоратор)
+
+
+
