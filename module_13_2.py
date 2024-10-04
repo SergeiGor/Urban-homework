d76b2437eb4869314873f66f0f4040d7da9dc0e4
SergeiGor <sgorobin@mail.ru>
2024-10-04 18:46:29 +0300

    Add files via upload

--- module_13_2.py
+++ module_13_2.py
@@ -7,9 +7,14 @@ api = '8050141538:AAEQjto5Qn5-TnOnqeJZ7E-QB88d90o4WVo'
 bot = Bot(token = api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-@dp.message_handler()
+@dp.message_handler(commands=['start'])
 async def start(message):
     print('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_massages(message):
+    print('Введите команду /start, чтобы начать общение.')
 
 
 if __name__ == '__main__':
@@ -17,8 +22,8 @@ if __name__ == '__main__':
 
 #
 # - печатает строку в консоли  . Запускается только когда написана команда '/start' в чате с ботом. (используйте соответствующий декоратор)
-# all_massages(message)
-# - печатает строку в консоли 'Введите команду /start, чтобы начать общение.'.
+# (message)
+# - печатает строку в консоли .
 # Запускается при любом обращении не описанном ранее. (используйте соответствующий декоратор)
 
 
